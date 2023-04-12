7ddaa3d479ece9538e74e1d32679f993b9cfc2e9
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-04-12 22:21:22 +0530

    the home page is half done

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Button from "./Button";
 function Landing() {
   return (
     <section className="sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8">
@@ -11,6 +11,13 @@ function Landing() {
           <span className="block">By Intellect</span>
           <span className="block">Driven By Value</span>
         </h1>
+        <div className="space-x-8">
+          <Button title="Buy Now" />
+          <a className="Link">Learn More</a>
+        </div>
+      </div>
+      <div className="relative hidden  h-[450px] w-[450px] transition-all duration-500 md:inline lg:h-[650px] lg:w-[650px] ">
+        <img src="/iphone.png" className="object-contain" />
       </div>
     </section>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,56 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Landing from "@/components/Landing";
 const inter = Inter({ subsets: ["latin"] });
+import { Tab } from "@headlessui/react";
+import { GetServerSideProps } from "next";
 export default function Home() {
   return (
     <div>
       <Header />
-      <main className="">
+      <main className="relative h-[200vh] bg-[#E7ECEE]">
         <Landing />
       </main>
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+        <div>
+          <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+            New Promos
+          </h1>
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              {/* {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg px-5 py-3 text-sm font-light outline-none md:px-6 md:py-4 md:text-base ${
+                      selected
+                        ? "borderGradient bg-[#35383C] text-white"
+                        : "border-b-2 border-[#35383C] text-[#747474]"
+                    }`
+                  }
+                >
+                  {category.title}
+                </Tab>
+              ))} */}
+            </Tab.List>
+            <Tab.Panels className="mx-auto max-w-fit pb-24 pt-10 sm:px-4">
+              {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </section>
     </div>
   );
 }
+
+// backend code
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const categories = await fetchCategories()
+  return {
+    props: {},
+  };
+};
