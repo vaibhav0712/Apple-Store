2dabd6bc33c722b8c70ff9ceacd62b7736719f95
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-06-19 18:21:58 +0530

    19-06-23 commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* <title>Apple redesign</title> */}
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
